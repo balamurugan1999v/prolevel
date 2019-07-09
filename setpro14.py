efa454e054f864c001e3892395d6dcf8190374a6
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2019-07-09 16:33:24 +0530

    Create setpro14.py

---
+++ setpro14.py
@@ -0,0 +1,19 @@
+n1,k1=input().split() 
+b=input().split()
+n=int(n1)
+k=int(k1)
+s1=[]
+s2=[]
+for i in b:
+  s1.append(int(i))
+for i in range(k):
+  u1,v1=input().split()
+  u=int(u1)
+  v=int(v1)
+  c=s1[u-1]
+  for j in range(u,v):
+    c=c^s1[j];
+  s2.append(c)
+for k in range(len(s2)):
+  print(s2[k])
+#
